e7ba5e9fde85f7cd5e0df94157a5125958945b0d
AlanQ <87281482+AlanDkoXS@users.noreply.github.com>
2024-11-24 20:11:59 -0600

    ⚡Update: Improve performance

--- assets/js/helpers/loader.js
+++ assets/js/helpers/loader.js
@@ -1,9 +1,15 @@
 const loaderContainer = document.querySelector('.loader');
+const spriteImage = document.querySelector('.home__sprite'); // Selecciona la imagen del sprite
 
 function loader() {
-	window.addEventListener('load', () => {
-		loaderContainer.classList.add('loader--hidden');
-	});
+  window.addEventListener('load', () => {
+    loaderContainer.classList.add('loader--hidden'); // Oculta el loader
+
+    // Después de que el loader se oculta, activamos la animación de la imagen
+    setTimeout(() => {
+      spriteImage.classList.add('animate'); // Inicia la animación
+    }, 500); // Le damos un pequeño retraso para asegurar que el loader se haya ocultado
+  });
 }
 
 export default loader;
