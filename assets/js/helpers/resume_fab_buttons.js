0f8e7d84c985be8388ac6bc41d4837d6ca2070d0
AlanQ <87281482+AlanDkoXS@users.noreply.github.com>
2024-12-13 13:04:42 -0600

    ⚡Update: Improve performance resume.html

--- assets/js/helpers/resume_fab_buttons.js
+++ assets/js/helpers/resume_fab_buttons.js
@@ -1,33 +1,81 @@
 function handleScroll() {
-    const fabButtons = document.querySelectorAll('.fab');
-    const pdfContainer = document.getElementById('pdf-container');
-    const btnDownload = document.getElementById('btn-download');
+  const fabButtons = document.querySelectorAll('.fab');
+  const pdfContainer = document.getElementById('pdf-container');
+  const btnDownload = document.getElementById('btn-download');
+  const modal = document.getElementById('modal');
 
-    const scrollTop = pdfContainer.scrollTop;
-    const scrollHeight = pdfContainer.scrollHeight;
-    const clientHeight = pdfContainer.clientHeight;
-    const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
+  const scrollTop = pdfContainer.scrollTop;
+  const scrollHeight = pdfContainer.scrollHeight;
+  const clientHeight = pdfContainer.clientHeight;
+  const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
 
-    if (scrollPercentage > 70) {
-      btnDownload.style.backgroundColor = 'green';
-    } else {
-      btnDownload.style.backgroundColor = '';
-    }
-
-    if (scrollTop > 20) {
-      fabButtons.forEach((button) => {
-        button.classList.add('expanded');
-      });
-    } else {
-      fabButtons.forEach((button) => {
-        button.classList.remove('expanded');
-      });
-    }
+  // Cambio de color del botón de descarga basado en el scroll
+  if (scrollPercentage > 70) {
+    btnDownload.style.backgroundColor = 'green';
+  } else {
+    btnDownload.style.backgroundColor = '';
   }
 
-  function initFab() {
-    const pdfContainer = document.getElementById('pdf-container');
-    pdfContainer.addEventListener('scroll', handleScroll);
+  // Expansión de los botones FAB
+  if (scrollTop > 20) {
+    fabButtons.forEach((button) => {
+      button.classList.add('expanded');
+    });
+  } else {
+    fabButtons.forEach((button) => {
+      button.classList.remove('expanded');
+    });
   }
+}
 
-  export default initFab;
+document.getElementById('btn-download').addEventListener('click', () => {
+  const modal = document.getElementById('modal');
+  if (!modal.open) {
+    openModal();
+  }
+});
+
+function openModal() {
+  const modal = document.getElementById('modal');
+  modal.showModal();
+
+  modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener(
+    'keydown',
+    (event) => {
+      if (event.key === 'Escape') {
+        closeModal();
+      }
+    },
+    { once: true }
+  );
+}
+
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.close();
+}
+
+function downloadCV(language) {
+  const fileName =
+    language === 'ENG' ? '[ENG] Alan Quintana.pdf' : '[ESP] Alan Quintana.pdf';
+  const link = document.createElement('a');
+  link.href = `/assets/docs/${fileName}`;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  closeModal();
+}
+
+function initFab() {
+  const pdfContainer = document.getElementById('pdf-container');
+  pdfContainer.addEventListener('scroll', handleScroll);
+}
+
+export { initFab, openModal, closeModal, downloadCV };
