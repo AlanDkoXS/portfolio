419086685b4e37f0b2d0a7ecb1c48d0fca6672b2
AlanQ <87281482+AlanDkoXS@users.noreply.github.com>
2024-12-13 00:53:21 -0600

    ✅ Update: Implementing auto dark mode

--- assets/js/helpers/send_form.js
+++ assets/js/helpers/send_form.js
@@ -3,29 +3,29 @@ const modalDOM = document.querySelector('#modal');
 const closeButton = document.querySelector('#btn_close-modal');
 
 function openModal() {
-	modalDOM.open = true;
+  modalDOM.setAttribute('open', '');
 }
 
 function closeModal() {
-	modalDOM.open = false;
+  modalDOM.removeAttribute('open');
 }
 
 function sendEmail() {
-	formDOM.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const params = {
-			name: document.getElementById('name').value,
-			email: document.getElementById('email').value,
-			subject: document.getElementById('subject').value,
-			message: document.getElementById('message').value,
-		};
+  formDOM.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const params = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      subject: document.getElementById('subject').value,
+      message: document.getElementById('message').value,
+    };
 
-		if (params.name && params.email && params.subject && params.message) {
-			emailjs
-				.send('service_i78o8xe', 'template_bzic089', params)
-				.then(openModal());
-		}
-	});
+    if (params.name && params.email && params.subject && params.message) {
+      emailjs
+        .send('service_i78o8xe', 'template_bzic089', params)
+        .then(() => openModal()); 
+    }
+  });
 }
 
 closeButton.addEventListener('click', closeModal);
