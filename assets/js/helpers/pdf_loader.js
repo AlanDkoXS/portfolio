cfc1b5c4b7a6cffabcecffc37392d5634d79b35e
AlanQ <87281482+AlanDkoXS@users.noreply.github.com>
2024-12-12 21:48:20 -0600

    ⚡Update: Improve performance

---
+++ assets/js/helpers/pdf_loader.js
@@ -0,0 +1,27 @@
+export function loadPdf() {
+    const url = '/assets/docs/resume.pdf';
+
+    pdfjsLib.getDocument(url).promise.then(function (pdf) {
+        const numPages = pdf.numPages;
+        for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+            pdf.getPage(pageNum).then(function (page) {
+                const scale = 1.5;
+                const viewport = page.getViewport({
+                    scale: scale
+                });
+
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                page.render({
+                    canvasContext: context,
+                    viewport: viewport
+                });
+
+                document.getElementById('pdf-container').appendChild(canvas);
+            });
+        }
+    });
+}
