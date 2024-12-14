974262d1045fe4e5354ee4f686dd3dae6731bea5
AlanQ <87281482+AlanDkoXS@users.noreply.github.com>
2024-12-14 16:09:46 -0600

    ⚡Update: Improve performance

--- assets/js/helpers/activeMenu.js
+++ assets/js/helpers/activeMenu.js
@@ -27,32 +27,6 @@ function activeMenu() {
       window.addEventListener('scroll', () => {
         navbarListDOM.classList.toggle('hidden', window.scrollY > 0);
       });
-
-      // Detectar clics fuera del menú para ocultarlo
-      const handleOutsideClick = (event) => {
-        if (!navbarListDOM.contains(event.target) && !event.target.closest('.navbar__link')) {
-          navbarListDOM.classList.add('hidden');
-        }
-      };
-
-      // Detectar toques fuera del menú
-      const handleTouchOutside = (event) => {
-        if (!navbarListDOM.contains(event.target) && !event.target.closest('.navbar__link')) {
-          navbarListDOM.classList.add('hidden');
-        }
-      };
-
-      // Agregar los listeners de clic y toque
-      document.addEventListener('mousedown', handleOutsideClick);
-      document.addEventListener('touchstart', handleTouchOutside);
-
-      // Limpiar los eventos cuando se cambia el tamaño de la ventana o se cierra el menú
-      window.addEventListener('resize', () => {
-        if (window.innerWidth > 768) {
-          document.removeEventListener('mousedown', handleOutsideClick);
-          document.removeEventListener('touchstart', handleTouchOutside);
-        }
-      });
     }
   };
 
