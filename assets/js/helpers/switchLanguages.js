a55c7ce07e56b830476fc970c77fc85dc5b068c9
AlanQ <87281482+AlanDkoXS@users.noreply.github.com>
2024-12-22 18:11:21 -0600

    ⚡Update: Improve performance

--- assets/js/helpers/switchLanguages.js
+++ assets/js/helpers/switchLanguages.js
@@ -13,18 +13,20 @@ const detectUserLanguage = () => {
     return (
       langCode.startsWith('es') ||
       langCode.includes('419') || // América Latina
-      langCode.includes('034')
-    ); // España
+      langCode.includes('034') // España
+    );
   });
 
   return isSpanishPreferred ? 'es' : 'en';
 };
 
+// Estado actual del idioma
 let currentLanguage = detectUserLanguage();
 
 // Exportar función para obtener mensajes de validación
 export const getValidationMessages = () => translations.validation[currentLanguage];
 
+// Función para actualizar textos en una sección específica
 const updateSectionText = (selector, key, lang) => {
   const element = document.querySelector(selector);
   if (element && translations[key]) {
@@ -32,6 +34,7 @@ const updateSectionText = (selector, key, lang) => {
   }
 };
 
+// Función para actualizar etiquetas de habilidades
 const updateSkillsTags = (selector, key, lang) => {
   const elements = document.querySelectorAll(selector);
   if (elements && translations[key]) {
@@ -41,7 +44,8 @@ const updateSkillsTags = (selector, key, lang) => {
   }
 };
 
-function updateProjectText(lang) {
+// Función para actualizar textos de proyectos
+const updateProjectText = (lang) => {
   const titles = document.querySelectorAll('.project__title');
   const descriptions = document.querySelectorAll('.project__description');
 
@@ -55,8 +59,9 @@ function updateProjectText(lang) {
         translations[descriptionKey][lang] || translations[descriptionKey]['en'];
     }
   });
-}
+};
 
+// Función principal para alternar idiomas
 const switchLanguages = () => {
   const languageToggle = document.getElementById('language-toggle');
 
@@ -64,100 +69,106 @@ const switchLanguages = () => {
   const initialLang = detectUserLanguage();
   languageToggle.checked = initialLang === 'es';
 
-  // Disparar el evento inicial para aplicar el idioma detectado
-  const event = new Event('change');
-  languageToggle.dispatchEvent(event);
+  // Aplicar el idioma detectado al cargar la página
+  applyLanguageChanges(initialLang);
 
+  // Escuchar el evento de cambio del toggle
   languageToggle.addEventListener('change', function () {
     const lang = this.checked ? 'es' : 'en';
-
-    const navLinks = [
-      { selector: '.navbar__link[href="#home"]', key: 'navHome' },
-      { selector: '.navbar__link[href="#about"]', key: 'navAbout' },
-      { selector: '.navbar__link[href="#skills"]', key: 'navSkills' },
-      { selector: '.navbar__link[href="#projects"]', key: 'navProjects' },
-      { selector: '.navbar__link[href="#contact"]', key: 'navContact' },
-    ];
-
-    navLinks.forEach((link) => updateSectionText(link.selector, link.key, lang));
-
-    // Home Section
-    updateSectionText('.home__title', 'homeTitle', lang);
-    updateSectionText('.home__description', 'homeDescription', lang);
-    updateSectionText('.btn.btn--primary[href="#about"]', 'aboutButton', lang);
-    updateSectionText('.btn.btn--primary[href="/assets/html/resume.html"]', 'resumeButton', lang);
-    updateSectionText('.btn.btn--primary[href="#skills"]', 'skillsButton', lang);
-    updateSectionText('.btn.btn--primary[href="#projects"]', 'projectButton', lang);
-
-    // About Section
-    updateSectionText('#about .section__title', 'aboutTitle', lang);
-    updateSectionText('.about__description', 'aboutDescription', lang);
-
-    // Skills Section
-    updateSectionText('#skills .section__title', 'skillsTitle', lang);
-    updateSectionText('.skills__description', 'skillsDescription', lang);
-    updateSectionText('.skills__tags-title', 'skillsTagsTitle', lang);
-    updateSkillsTags('.skills__tag', 'skillsTags', lang);
-    updateSectionText('.skills__title', 'skillsDesignTools', lang);
-    updateSectionText('.skills__frontend-tools-title', 'skillsFrontendTools', lang);
-    updateSectionText('.skills__frontend-tools-title', 'skillsBackendTools', lang);
-    updateSectionText('.skills__development-tools-title', 'skillsDevelopmentTools', lang);
-
-    // Project Section
-    updateSectionText('#projects .section__title', 'projectsTitle', lang);
-    updateProjectText(lang);
-
-    // Contact Section
-    updateSectionText('#contact .section__title', 'contactTitle', lang);
-    updateSectionText('.contact__info-title', 'contactInfoTitle', lang);
-    updateSectionText('.contact__info-description', 'contactInfoDescription', lang);
-    updateSectionText('.contact__form-title', 'contactFormTitle', lang);
-
-    // Form Labels
-    const formElements = {
-      name: document.querySelector('.contact__form-label[for="name"]'),
-      email: document.querySelector('.contact__form-label[for="email"]'),
-      subject: document.querySelector('.contact__form-label[for="subject"]'),
-      message: document.querySelector('.contact__form-label[for="message"]'),
-    };
-
-    Object.entries(formElements).forEach(([key, element]) => {
-      if (element && translations.contactForm[lang][key]) {
-        element.textContent = translations.contactForm[lang][key];
-      }
-    });
-
-    // Submit Button
-    const submitButton = document.querySelector('.contact__form-button');
-    if (submitButton) {
-      submitButton.innerHTML = `<strong>${translations.contactForm[lang].send}</strong>`;
-    }
-
-    // Language Change Event
-    window.dispatchEvent(
-      new CustomEvent('languageChange', {
-        detail: {
-          language: lang,
-          validationMessages: translations.validation[lang],
-        },
-      }),
-    );
-
-    // Modal Updates
-    const modalText = document.querySelector('#modal .modal-content p');
-    if (modalText) {
-      modalText.textContent = translations.contactForm[lang].formSuccess;
-    }
-
-    const closeButton = document.querySelector('#btn_close-modal strong');
-    if (closeButton) {
-      closeButton.textContent = translations.contactForm[lang].close;
-    }
-
-    // Footer Section
-    updateSectionText('.footer__description', 'footerDescription', lang);
-    updateSectionText('.footer__copyright', 'footerCopyright', lang);
+    applyLanguageChanges(lang);
   });
+};
+
+// Función para aplicar los cambios de idioma
+const applyLanguageChanges = (lang) => {
+  currentLanguage = lang; // Actualizar el estado global del idioma
+
+  const navLinks = [
+    { selector: '.navbar__link[href="#home"]', key: 'navHome' },
+    { selector: '.navbar__link[href="#about"]', key: 'navAbout' },
+    { selector: '.navbar__link[href="#skills"]', key: 'navSkills' },
+    { selector: '.navbar__link[href="#projects"]', key: 'navProjects' },
+    { selector: '.navbar__link[href="#contact"]', key: 'navContact' },
+  ];
+
+  navLinks.forEach((link) => updateSectionText(link.selector, link.key, lang));
+
+  // Home Section
+  updateSectionText('.home__title', 'homeTitle', lang);
+  updateSectionText('.home__description', 'homeDescription', lang);
+  updateSectionText('.btn.btn--primary[href="#about"]', 'aboutButton', lang);
+  updateSectionText('.btn.btn--primary[href="/assets/html/resume.html"]', 'resumeButton', lang);
+  updateSectionText('.btn.btn--primary[href="#skills"]', 'skillsButton', lang);
+  updateSectionText('.btn.btn--primary[href="#projects"]', 'projectButton', lang);
+
+  // About Section
+  updateSectionText('#about .section__title', 'aboutTitle', lang);
+  updateSectionText('.about__description', 'aboutDescription', lang);
+
+  // Skills Section
+  updateSectionText('#skills .section__title', 'skillsTitle', lang);
+  updateSectionText('.skills__description', 'skillsDescription', lang);
+  updateSectionText('.skills__tags-title', 'skillsTagsTitle', lang);
+  updateSkillsTags('.skills__tag', 'skillsTags', lang);
+  updateSectionText('.skills__title', 'skillsDesignTools', lang);
+  updateSectionText('.skills__frontend-tools-title', 'skillsFrontendTools', lang);
+  updateSectionText('.skills__backend-tools-title', 'skillsBackendTools', lang);
+  updateSectionText('.skills__development-tools-title', 'skillsDevelopmentTools', lang);
+
+  // Project Section
+  updateSectionText('#projects .section__title', 'projectsTitle', lang);
+  updateProjectText(lang);
+
+  // Contact Section
+  updateSectionText('#contact .section__title', 'contactTitle', lang);
+  updateSectionText('.contact__info-title', 'contactInfoTitle', lang);
+  updateSectionText('.contact__info-description', 'contactInfoDescription', lang);
+  updateSectionText('.contact__form-title', 'contactFormTitle', lang);
+
+  // Form Labels
+  const formElements = {
+    name: document.querySelector('.contact__form-label[for="name"]'),
+    email: document.querySelector('.contact__form-label[for="email"]'),
+    subject: document.querySelector('.contact__form-label[for="subject"]'),
+    message: document.querySelector('.contact__form-label[for="message"]'),
+  };
+
+  Object.entries(formElements).forEach(([key, element]) => {
+    if (element && translations.contactForm[lang][key]) {
+      element.textContent = translations.contactForm[lang][key];
+    }
+  });
+
+  // Submit Button
+  const submitButton = document.querySelector('.contact__form-button');
+  if (submitButton) {
+    submitButton.innerHTML = `<strong>${translations.contactForm[lang].send}</strong>`;
+  }
+
+  // Language Change Event
+  window.dispatchEvent(
+    new CustomEvent('languageChange', {
+      detail: {
+        language: lang,
+        validationMessages: translations.validation[lang],
+      },
+    }),
+  );
+
+  // Modal Updates
+  const modalText = document.querySelector('#modal .modal-content p');
+  if (modalText) {
+    modalText.textContent = translations.contactForm[lang].formSuccess;
+  }
+
+  const closeButton = document.querySelector('#btn_close-modal strong');
+  if (closeButton) {
+    closeButton.textContent = translations.contactForm[lang].close;
+  }
+
+  // Footer Section
+  updateSectionText('.footer__description', 'footerDescription', lang);
+  updateSectionText('.footer__copyright', 'footerCopyright', lang);
 };
 
 export default switchLanguages;
